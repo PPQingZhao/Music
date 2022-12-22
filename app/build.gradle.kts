eb1e245893f3ec021d7a03592cea22c361b230c1
PPQingZhao <2938672226@qq.com>
2022-12-22 18:07:01 +0800

    1.添加libs.versions.toml 2.gradle改kotlin版本

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+@file:Suppress("UnstableApiUsage", "DSL_SCOPE_VIOLATION")
+
+plugins {
+    alias(libs.plugins.kotlin.android)
+    id("com.android.application") version (libs.versions.androidGradlePlugin)
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.pp.music"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.pp.music"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = libs.versions.versionCode.get().toInt()
+        versionName = libs.versions.versionName.get()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        multiDexEnabled = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    dataBinding {
+        enable = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,43 @@
+@file: Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.aliyun.com/repository/public")
+//        maven("https://maven.google.com")
+//        maven("https://jitpack.io")
+    }
+}
+dependencyResolutionManagement {
+//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven("https://maven.aliyun.com/repository/public")
+//        maven("https://jitpack.io")
+        google()
+        mavenCentral()
+    }
+}
+
+enableFeaturePreview("VERSION_CATALOGS")
+
+// Generate type safe accessors when referring to other projects eg.
+// Before: implementation(project(":feature-album"))
+// After: implementation(projects.featureAlbum)
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "Music"
+
+include(
+        ":app",
+//        ":library-mvvm",
+//        ":library-base",
+//        ":library-ui",
+//        ":library-router-service",
+//        ":library-network",
+//        ":library-common",
+//        ":library-database",
+//        ":library-theme",
+//        ":module-main",
+)
